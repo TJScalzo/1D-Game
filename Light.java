05e11c0a9b4e4ddb562cdba1bc3e64818bbddfaa
TJScalzo <timtv25@gmail.com>
2017-02-27 09:41:38 -0500

    Created Light class

---
+++ Light.java
@@ -0,0 +1,26 @@
+public class Light
+{
+    private boolean isOn;
+    
+    //Constructors
+    public Light()
+    {
+        isOn = false;
+    }
+    
+    public Light(boolean isOn)
+    {
+        this.isOn = isOn;
+    }
+    
+    //Methods
+    public boolean returnIsOn()
+    {
+        return isOn;
+    }
+    
+    public void toggle()
+    {
+        isOn = !isOn;
+    }
+}
