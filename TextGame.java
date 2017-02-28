5db3d691c16ff3b0779642dd1225b5607d7fa52e
TJScalzo <timtv25@gmail.com>
2017-02-28 09:47:54 -0500

    Started text version of game

--- TextGame.java
+++ TextGame.java
@@ -1,3 +1,15 @@
-public class TextGame{
-    
+import java.util.Scanner;
+
+public class TextGame {
+    public void startGame() {
+        Scanner input = new Scanner(System.in); //creates scanner object
+        String response = "";
+
+        System.out.println("How many lights do you want in the row?");
+        int numLights = input.nextInt();
+        LightRow gameRow = new LightRow(numLights);
+        for (int i = 0; i < gameRow.length(); i++) {
+            
+        }
+    }
 }
