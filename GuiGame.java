ecd04510a784a7733c6302b119b9b0fc67b5ad62
Omar Ahmad <eatmorerice@outlook.com>
2017-03-02 09:48:04 -0500

    added more testing

---
+++ GuiGame.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class GridPaneTest extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Lights Out!");
+        
+        
+    }
+    
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+}
